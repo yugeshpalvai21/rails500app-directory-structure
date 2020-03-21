5bfa40e5e69560a1f0be92366cdba4f3f1e96347
Yugesh Palvai <pyreddy517@hotmail.com>
2020-03-21 16:16:48 +0530

    Explore Devise Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'pages#home'
+  
+  devise_for :users, path: 'auth', path_names: {sign_in: 'login', sign_out: 'logout'}
+  
+  # devise_scope :user do
+  #   get '/login', to: 'devise/sessions#new'
+  #   post '/login', to: 'devise/sessions#create'
+  #   delete '/logout', to: 'devise/sessions#destroy'
+  # end
+  
+  #devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+ 
+  unauthenticated do
+    root 'pages#home'
+  end
+
+  authenticated do
+    root 'pages#greeting'
+  end
+  
+  scope :admin do
+    get '/hello', to: 'pages#home'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
